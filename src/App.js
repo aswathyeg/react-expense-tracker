e68bbff2528cf7d305d2c00baa92d57d74078f4f
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-01-11 22:45:36 +0530

    separated header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import "./App.css";
 import Balance from "./components/Balance";
 import Header from "./components/Header";
 import IncomeExpense from "./components/IncomeExpense";
+import History from "./components/History";
+import AddTransaction from "./components/AddTransaction";
 function App() {
   return (
     <div className="App">
       <Header />
-      <Balance />
-      <IncomeExpense />
+      <div className="expense">
+        <Balance />
+        <IncomeExpense />
+        <History />
+        <AddTransaction />
+      </div>
     </div>
   );
 }
