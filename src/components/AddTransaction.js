9d1d2c34bf6e03453b7acb566ab5994bfbc9b35d
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-01-11 22:45:57 +0530

    added form

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,7 +1,27 @@
 import React from "react";
-
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 const AddTransaction = () => {
-  return <div>AddTransaction</div>;
+  return (
+    <div>
+      <h5>Add New Transaction</h5>
+      <Form className="expenseForm">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Text</Form.Label>
+          <Form.Control type="email" placeholder="Enter text.." />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Amount (negative-Expense,positive-Income)</Form.Label>
+          <Form.Control type="number" placeholder="Enter Amount" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 };
 
 export default AddTransaction;
