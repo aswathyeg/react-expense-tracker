f4d13670b03bea940186f3c3977d1355d1d921f2
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-01-11 19:49:57 +0530

    added more components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import Balance from "./components/Balance";
 import Header from "./components/Header";
-
+import IncomeExpense from "./components/IncomeExpense";
 function App() {
   return (
     <div className="App">
       <Header />
+      <Balance />
+      <IncomeExpense />
     </div>
   );
 }
