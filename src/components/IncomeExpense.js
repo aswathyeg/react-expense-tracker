3387a67d4d3893657ae0d098a88aa9e66012abe2
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-01-11 19:50:27 +0530

    added Cards

--- src/components/IncomeExpense.js
+++ src/components/IncomeExpense.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
 const IncomeExpense = () => {
-  return <div>IncomeExpense</div>;
+  return (
+    <div className="ieCard">
+      <Card>Income</Card>
+      <Card>Expense</Card>
+    </div>
+  );
 };
 
 export default IncomeExpense;
