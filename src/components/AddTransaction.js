7ea35faf62d6b45e9184bda2ce6156c9a406dad6
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-01-13 11:25:51 +0530

    added state

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 const AddTransaction = () => {
+  const [text, seText] = useState("");
+  const [amount, setAmount] = useState(0);
+  const handleText = (e) => {
+    seText(e.target.value);
+  };
+  const handleAmount = (e) => {
+    setAmount(e.target.value);
+  };
   return (
     <div>
       <h5>Add New Transaction</h5>
       <Form className="expenseForm">
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Text</Form.Label>
-          <Form.Control type="email" placeholder="Enter text.." />
+          <Form.Control
+            type="text"
+            placeholder="Enter text.."
+            value={text}
+            onChange={handleText}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Amount (negative-Expense,positive-Income)</Form.Label>
-          <Form.Control type="number" placeholder="Enter Amount" />
+          <Form.Control
+            type="number"
+            placeholder="Enter Amount"
+            value={amount}
+            onChange={handleAmount}
+          />
         </Form.Group>
 
         <Button className="addButton" type="submit">
