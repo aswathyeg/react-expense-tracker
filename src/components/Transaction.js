70d948d41ecd5463e595d65d26cf91f9c5d65103
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-01-19 17:25:47 +0530

    updated

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import ListGroup from "react-bootstrap/ListGroup";
+import Button from "react-bootstrap/Button";
+function convertMoney(num) {
+  let p = num.toFixed(2).split(".");
+  return (
+    "$" +
+    p[0]
+      .split("")
+      .reverse()
+      .reduce(function (acc, num, i, orig) {
+        return num === "-" ? acc : num + (i && !(i % 3) ? "," : "") + acc;
+      }, "") +
+    "." +
+    p[1]
+  );
+}
+export const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
 
-const Transaction = () => {
-  return <div>Transaction</div>;
+  const sign = transaction.amount > 0 ? "+" : "-";
+
+  return (
+    <ListGroup.Item className={transaction.amount < 0 ? "minus" : "plus"}>
+      {/* <li className={transaction.amount > 0 ? "plus" : "minus"}> */}
+      {transaction.text}
+      <span>
+        {sign}
+        {convertMoney(transaction.amount)}
+      </span>{" "}
+      <Button onClick={() => deleteTransaction(transaction.id)}>x</Button>
+    </ListGroup.Item>
+  );
 };
 
-export default Transaction;
+// export default Transaction;
