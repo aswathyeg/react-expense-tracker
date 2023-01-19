9e421259f7df8bca5a7ca83b8126a668d7cb4b55
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-01-19 17:20:52 +0530

    updated components

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { GlobalProvider } from "./context/GlobalState";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <GlobalProvider>
     <App />
-  </React.StrictMode>
+  </GlobalProvider>
 );
