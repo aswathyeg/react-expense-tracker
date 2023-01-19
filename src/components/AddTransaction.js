3b8614f856e13beeca130cf587686df89163dccb
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-01-19 17:23:39 +0530

    added state

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,26 +1,25 @@
 import React, { useState, useContext } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { ExpenseContext } from "../context/GlobalState";
+import { GlobalContext } from "../context/GlobalState";
 const AddTransaction = () => {
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
-  const { addTransaction } = useContext(ExpenseContext);
-  const handleText = (e) => {
-    setText(e.target.value);
-  };
-  const handleAmount = (e) => {
-    setAmount(e.target.value);
-  };
+  const { addTransaction } = useContext(GlobalContext);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    setText("");
+    setAmount("");
     const newTransation = {
       id: Math.floor(Math.random() * 100000000),
       text,
       amount: +amount,
     };
     addTransaction(newTransation);
+    //console.log(addTransaction(newTransation));
   };
+
   return (
     <div>
       <h5>Add New Transaction</h5>
@@ -31,7 +30,7 @@ const AddTransaction = () => {
             type="text"
             placeholder="Enter text.."
             value={text}
-            onChange={handleText}
+            onChange={(e) => setText(e.target.value)}
           />
         </Form.Group>
 
@@ -41,7 +40,7 @@ const AddTransaction = () => {
             type="number"
             placeholder="Enter Amount"
             value={amount}
-            onChange={handleAmount}
+            onChange={(e) => setAmount(e.target.value)}
           />
         </Form.Group>
 
