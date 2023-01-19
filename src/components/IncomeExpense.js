055c81371cb50966cb44400a109c22856c635231
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-01-19 17:25:13 +0530

    updated

--- src/components/IncomeExpense.js
+++ src/components/IncomeExpense.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Card } from "react-bootstrap";
+import { GlobalContext } from "../context/GlobalState";
+
+function moneyFormatter(num) {
+  let p = num.toFixed(2).split(".");
+  return (
+    " $ " +
+    p[0]
+      .split("")
+      .reverse()
+      .reduce(function (acc, num, i, orig) {
+        return num === "-" ? acc : num + (i && !(i % 3) ? "," : "") + acc;
+      }, "") +
+    "." +
+    p[1]
+  );
+}
 
 const IncomeExpense = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amount = transactions.map((transaction) => transaction.amount);
+
+  const income = amount
+    .filter((income) => income > 0)
+    .reduce((acc, num) => (acc = +num), 0);
+
+  const expense =
+    amount.filter((item) => item < 0).reduce((acc, num) => acc + num, 0) * -1;
+
   return (
     <div className="ieCard">
-      <Card>Income</Card>
-      <Card>Expense</Card>
+      <Card style={{ backgroundColor: "green" }}>
+        Income {moneyFormatter(income)}
+      </Card>
+      <Card style={{ backgroundColor: "red" }}>
+        Expense {moneyFormatter(expense)}
+      </Card>
     </div>
   );
 };
